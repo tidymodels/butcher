de1c841aaa7c55f4378ca7bbed2070ad9889b2ce
jyuu <jyu21@ncsu.edu>
2019-06-17 11:27:46 -0400

    helper added as done in readlx to access the example model objects

---
+++ R/example.R
@@ -0,0 +1,17 @@
+#' Get path to model object example.
+#'
+#' butcher comes bundled with some example files in its `inst/extdata`
+#' directory. This function was copied from readxl and placed here to
+#' make the instantiated model objects easy to access.
+#'
+#' @param path Name of file. If `NULL`, the example files will be listed.
+#' @examples
+#' butcher_example()
+#' butcher_example("lm.rda")
+butcher_example <- function(path = NULL) {
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "butcher"))
+  } else {
+    system.file("extdata", path, package = "butcher", mustWork = TRUE)
+  }
+}

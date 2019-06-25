35a902e371b9f2d98abc4d602eca123da2fc55e0
jyuu <jyu21@ncsu.edu>
2019-06-25 14:23:51 -0400

    Updated documentation for formula objects

--- R/formula.R
+++ R/formula.R
@@ -7,22 +7,6 @@
 #'
 #' @return axed formula
 #'
-#' @examples
-#' # Load libraries
-#' suppressWarnings(suppressMessages(library(caret)))
-#'
-#' data(iris)
-#' train_data <- iris[, 1:4]
-#' train_classes <- iris[, 5]
-#'
-#' train_fit <- train(train_data, train_classes,
-#'                    method = "knn",
-#'                    preProcess = c("center", "scale"),
-#'                    tuneLength = 10,
-#'                    trControl = trainControl(method = "cv"))
-#'
-#' axe_env(train_fit$modelInfo$prob)
-#'
 #' @name axe-formula
 NULL
 

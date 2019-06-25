59125af14237296b28ccd179a50b017b0e21a5c6
jyuu <jyu21@ncsu.edu>
2019-06-25 08:12:13 -0400

    Updated axe functions and tests for train object to remove additional environments as a result of srcrefs

---
+++ R/function.R
@@ -0,0 +1,36 @@
+#' Axing functions.
+#'
+#' functions often wrap environments that carry a lot of unneccesary junk.
+#'
+#' @param x function
+#' @param ... any additional arguments related to axing
+#'
+#' @return axed function
+#'
+#' @examples
+#' # Load libraries
+#' suppressWarnings(suppressMessages(library(caret)))
+#'
+#' data(iris)
+#' train_data <- iris[, 1:4]
+#' train_classes <- iris[, 5]
+#'
+#' train_fit <- train(train_data, train_classes,
+#'                    method = "knn",
+#'                    preProcess = c("center", "scale"),
+#'                    tuneLength = 10,
+#'                    trControl = trainControl(method = "cv"))
+#'
+#' axe_env(train_fit$modelInfo$prob)
+#'
+#' @name axe-function
+NULL
+
+#' Remove the environment.
+#'
+#' @rdname axe-function
+#' @export
+axe_env.function <- function(x, ...) {
+  attributes(x) <- NULL
+  x
+}

7c2d1d9071f55bfbfff3527e00250dea91f0c81a
jyuu <jyu21@ncsu.edu>
2019-06-25 14:17:18 -0400

    Updated sub axe functions, tests and documentation for ranger objects

--- tests/testthat/test-ranger.R
+++ tests/testthat/test-ranger.R
@@ -16,3 +16,11 @@ test_that("ranger + predict() works", {
   expect_equal(predict(x, new_data)$predictions[1,1], c(setosa = 1))
   expect_equal(predict(x, data = new_data, predict.all = TRUE)$predictions[1,1,20], 1)
 })
+
+load(butcher_example("ranger_reg.rda"))
+
+test_that("ranger_reg + predict() works", {
+  x <- butcher(ranger_reg_fit)
+  new_data <- iris[1:3, ]
+  expect_equal(predict(x, new_data)$predictions, c(3.57547064990565, 3.20695107226107, 3.20450937229437))
+})

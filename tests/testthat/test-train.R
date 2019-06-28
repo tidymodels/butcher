9400e013169ba7f9a2957c8045bfc657facc24c3
jyuu <jyu21@ncsu.edu>
2019-06-28 12:27:55 -0400

    Updated tests for caret train object

--- tests/testthat/test-train.R
+++ tests/testthat/test-train.R
@@ -1,16 +1,24 @@
 context("train")
 
-library(caret)
-
-test_that("train + butcher_example() works", {
-  example_files <- butcher_example()
-  expect_true("train.rda" %in% example_files)
-  expect_true(file.exists(butcher_example("train.rda")))
-})
-
-load(butcher_example("train.rda"))
+# Train objects are large so create on the fly
+# test_that("train + butcher_example() works", {
+#   example_files <- butcher_example()
+#   expect_true("train.rda" %in% example_files)
+#   expect_true(file.exists(butcher_example("train.rda")))
+# })
 
 test_that("train + predict() works", {
+  skip_on_cran()
+  skip_if_not_installed("caret")
+  # Model
+  train_data <- iris[, 1:4]
+  train_classes <- iris[, 5]
+  train_fit <- caret::train(train_data, train_classes,
+                     method = "knn",
+                     preProcess = c("center", "scale"),
+                     tuneLength = 10,
+                     trControl = caret::trainControl(method = "cv"))
+  # Butcher
   x <- butcher(train_fit)
   # Load testing data
   test_data <- iris[1:3, 1:4]

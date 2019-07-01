bd7a052b7caf60344a215a101b298d2c24ccb5ea
jyuu <jyu21@ncsu.edu>
2019-07-01 09:52:32 -0400

    Updated tests to make stan objects on the fly

--- tests/testthat/test-stanreg.R
+++ tests/testthat/test-stanreg.R
@@ -1,45 +1,32 @@
 context("stanreg")
 
-test_that("stanreg + butcher_example() works", {
-  example_files <- butcher_example()
-  expect_true("stanreg.rda" %in% example_files)
-  expect_true(file.exists(butcher_example("stanreg.rda")))
-})
+# Stan objects are too large create on the fly
 
-load(butcher_example("stanreg.rda"))
+# test_that("stanreg + butcher_example() works", {
+#   example_files <- butcher_example()
+#   expect_true("stanreg.rda" %in% example_files)
+#   expect_true(file.exists(butcher_example("stanreg.rda")))
+# })
 
-test_that("stanreg + axe_call() works", {
-  x <- axe_call(stanreg_fit)
-  expect_equal(x$call, rlang::expr(dummy_call()))
-})
-
-test_that("stanreg + axe_env() works", {
-  x <- axe_env(stanreg_fit)
-  test_en <- rlang::base_env()
-  expect_identical(attr(x$terms, ".Environment"), test_en)
-  expect_identical(attr(attributes(x$model)$terms, ".Environment"), test_en)
-  expect_identical(x$stanfit@.MISC, test_en)
-  expect_identical(x$stanfit@stanmodel@dso@.CXXDSOMISC, test_en)
-})
-
-test_that("stanreg + axe_fitted() works", {
-  x <- axe_fitted(stanreg_fit)
-  expect_equal(x$fitted.values, numeric(0))
-})
+# load(butcher_example("stanreg.rda"))
 
 test_that("stanreg + butcher() works", {
+  skip_on_cran()
+  skip_if_not_installed("rstanarm")
+  skip_if_not_installed("parsnip")
+  library(parsnip)
+  ctrl <- fit_control(verbosity = 0) # Avoid printing output
+  stanreg_fit <- linear_reg() %>%
+    set_engine("stan") %>%
+    fit(mpg ~ ., data = mtcars, control = ctrl)
   x <- butcher(stanreg_fit)
   expect_equal(x$call, rlang::expr(dummy_call()))
-  test_en <- rlang::base_env()
-  expect_identical(attr(x$terms, ".Environment"), test_en)
-  expect_identical(attr(attributes(x$model)$terms, ".Environment"), test_en)
-  expect_identical(x$stanfit@.MISC, test_en)
-  expect_identical(x$stanfit@stanmodel@dso@.CXXDSOMISC, test_en)
+  expect_identical(attr(x$terms, ".Environment"), rlang::base_env())
+  expect_identical(attr(attributes(x$model)$terms, ".Environment"), rlang::base_env())
+  expect_identical(x$stanfit@.MISC, rlang::empty_env())
+  expect_identical(x$stanfit@stanmodel@dso@.CXXDSOMISC, rlang::empty_env())
   expect_equal(x$fitted.values, numeric(0))
   # expect_equal(class(x)[1], "butcher_stanreg") # TODO: Fix class assignment
-})
-
-test_that("stanreg + predict() works", {
-  x <- butcher(stanreg_fit)
-  expect_gt(predict(x)[1], 20)
+  expected_output <- predict(stanreg_fit$fit)
+  expect_equal(predict(x), expected_output)
 })

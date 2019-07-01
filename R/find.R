6992edc69e0d57fbc93ae3b021d84c47b7ad36ab
jyuu <jyu21@ncsu.edu>
2019-07-01 09:32:43 -0400

    Possible locate function

---
+++ R/find.R
@@ -0,0 +1,9 @@
+#' Find an object.
+#'
+#' Locate where a specific component of a object might exist.
+#'
+#' @param x model object
+#' @param ... any additional arguments related to locating a part of an object
+#'
+
+# TODO: recursive function to check for an object location..

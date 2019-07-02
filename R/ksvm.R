784cf03eac0fbccaf851910e1b6a1edfb69ca4d2
jyuu <jyu21@ncsu.edu>
2019-07-02 09:12:27 -0400

    No axing functions for ksvm given new butcher class assignment would remove required S4 functionality

---
+++ R/ksvm.R
@@ -0,0 +1,26 @@
+#' Axing a ksvm object.
+#'
+#' This is where all the ksvm specific documentation lies.
+#'
+#' @param x model object
+#' @param ... any additional arguments related to axing
+#'
+#' @return axed model object
+#'
+#' @examples
+#' # Load libraries
+#' suppressWarnings(suppressMessages(library(parsnip)))
+#'
+#' # Load data
+#' data(spam)
+#'
+#' # Create model and fit
+#' ksvm_class <- svm_poly(mode = "classification") %>%
+#'   set_engine("kernlab", kernel = "rbfdot") %>%
+#'   fit(type ~ ., data = spam)
+#'
+#' butcher(ksvm_class)
+#'
+#' @name axe-ksvm
+NULL
+

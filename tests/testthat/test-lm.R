0c2d1a2c7252e80dbef687dc511dadc9aad977dd
jyuu <jyu21@ncsu.edu>
2019-07-15 12:52:01 -0400

    Updated axe methods, tests, docs for lm

--- tests/testthat/test-lm.R
+++ tests/testthat/test-lm.R
@@ -15,7 +15,7 @@ test_that("lm + axe_call() works", {
 
 test_that("lm + axe_env() works", {
   x <- axe_env(lm_fit)
-  expect_identical(attr(x$terms, ".Environment"), rlang::base_env())
+  expect_identical(attr(x$terms, ".Environment"), rlang::empty_env())
 })
 
 test_that("lm + axe_fitted() works", {
@@ -26,7 +26,7 @@ test_that("lm + axe_fitted() works", {
 test_that("lm + butcher() works", {
   x <- butcher(lm_fit)
   expect_equal(x$call, rlang::expr(dummy_call()))
-  expect_identical(attr(x$terms, ".Environment"), rlang::base_env())
+  expect_identical(attr(x$terms, ".Environment"), rlang::empty_env())
   expect_equal(x$fitted.values, numeric(0))
   expect_equal(class(x)[1], "butchered_lm")
 })
@@ -34,4 +34,10 @@ test_that("lm + butcher() works", {
 test_that("lm + predict() works", {
   x <- butcher(lm_fit)
   expect_equal(predict(x)[1], c(`Mazda RX4` = 21.5647055857078))
+  set.seed(0); w <- runif(nrow(trees), 1, 2)
+  X <- model.matrix(~ Girth + Volume, trees)
+  y <- trees$Height
+  fit_by_wls <- lm(y ~ X - 1, weights = w)
+  x <- axe_env(fit_by_wls)
+  expect_lt(lobstr::obj_size(fit_by_wls), lobstr::obj_size(x))
 })

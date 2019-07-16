09a3f68ec071f900f93e71a485791c30a07f1336
jyuu <jyu21@ncsu.edu>
2019-07-16 19:58:56 -0400

    Updated survreg methods, tests, docs based on new butcher attribution

--- R/survreg.R
+++ R/survreg.R
@@ -1,14 +1,17 @@
 #' Axing an survreg.
 #'
-#' survreg objects are created from the \code{survival} package. They
+#' survreg objects are created from the \pkg{survival} package. They
 #' are returned from the \code{survreg} function, representing fitted
 #' parametric survival models. This is where all the survreg specific
 #' documentation lies.
 #'
-#' @param x model object
-#' @param ... any additional arguments related to axing
+#' @param x Model object.
+#' @param verbose Print information each time an axe method is executed
+#'  that notes how much memory is released and what functions are
+#'  disabled. Default is \code{TRUE}.
+#' @param ... Any additional arguments related to axing.
 #'
-#' @return axed model object
+#' @return Axed model object.
 #'
 #' @examples
 #' # Load libraries
@@ -29,28 +32,39 @@ NULL
 #'
 #' @rdname axe-survreg
 #' @export
-axe_call.survreg <- function(x, ...) {
+axe_call.survreg <- function(x, verbose = TRUE, ...) {
+  old <- x
   x$call <- call("dummy_call")
-  add_butcher_class(x)
+
+  add_butcher_attributes(x, old,
+                         disabled = c("print", "summary"),
+                         verbose = verbose)
 }
 
 #' Remove the data.
 #'
 #' @rdname axe-survreg
 #' @export
-axe_data.survreg <- function(x, ...) {
+axe_data.survreg <- function(x, verbose = TRUE, ...) {
+  old <- x
   x$y <- numeric(0)
-  add_butcher_class(x)
+
+  add_butcher_attributes(x, old,
+                         disabled = c("residuals"),
+                         verbose = verbose)
 }
 
 #' Remove environments.
 #'
 #' @rdname axe-survreg
 #' @export
-axe_env.survreg <- function(x, ...) {
+axe_env.survreg <- function(x, verbose = TRUE, ...) {
+  old <- x
   x$terms <- axe_env(x$terms, ...)
   attributes(x$model)$terms <- axe_env(attributes(x$model)$terms, ...)
-  add_butcher_class(x)
+
+  add_butcher_attributes(x, old,
+                         verbose = verbose)
 }
 
 

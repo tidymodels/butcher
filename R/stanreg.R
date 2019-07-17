a91ed1f12aa60b0947b5ca3ee38f8f5225a5fc83
jyuu <jyu21@ncsu.edu>
2019-07-17 07:17:18 -0400

    Updated stanreg methods and tests to reflect new butcher attributes

--- R/stanreg.R
+++ R/stanreg.R
@@ -2,10 +2,13 @@
 #'
 #' This is where all the stanreg specific documentation lies.
 #'
-#' @param x model object
-#' @param ... any additional arguments related to axing
+#' @param x Model object.
+#' @param verbose Print information each time an axe method is executed
+#'  that notes how much memory is released and what functions are
+#'  disabled. Default is \code{TRUE}.
+#' @param ... Any additional arguments related to axing.
 #'
-#' @return axed model object
+#' @return Axed model object.
 #'
 #' @name axe-stanreg
 NULL
@@ -14,30 +17,39 @@ NULL
 #'
 #' @rdname axe-stanreg
 #' @export
-axe_call.stanreg <- function(x, ...) {
+axe_call.stanreg <- function(x, verbose = TRUE, ...) {
+  old <- x
   x$call <- call("dummy_call")
-  add_butcher_class(x)
+
+  add_butcher_attributes(x, old,
+                         verbose = verbose)
 }
 
 #' Remove environments.
 #'
 #' @rdname axe-stanreg
 #' @export
-axe_env.stanreg <- function(x, ...) {
+axe_env.stanreg <- function(x, verbose = TRUE, ...) {
+  old <- x
   x$stanfit@.MISC <- rlang::empty_env()
   x$stanfit@stanmodel@dso@.CXXDSOMISC <- rlang::empty_env()
   x$terms <- axe_env(x$terms, ...)
   attributes(x$model)$terms <- axe_env(attributes(x$model)$terms, ...)
-  add_butcher_class(x)
+
+  add_butcher_attributes(x, old,
+                         verbose = verbose)
 }
 
 #' Remove fitted values.
 #'
 #' @rdname axe-stanreg
 #' @export
-axe_fitted.stanreg <- function(x, ...) {
+axe_fitted.stanreg <- function(x, verbose = TRUE, ...) {
+  old <- x
   x$fitted.values <- numeric(0)
-  add_butcher_class(x)
+
+  add_butcher_attributes(x, old,
+                         verbose = verbose)
 }
 
 
--- tests/testthat/test-stanreg.R
+++ tests/testthat/test-stanreg.R
@@ -9,13 +9,16 @@ test_that("stanreg + butcher() works", {
   stanreg_fit <- linear_reg() %>%
     set_engine("stan") %>%
     fit(mpg ~ ., data = mtcars, control = ctrl)
-  x <- butcher(stanreg_fit)
+  x <- axe_call(stanreg_fit)
   expect_equal(x$call, rlang::expr(dummy_call()))
+  x <- axe_env(stanreg_fit)
   expect_identical(attr(x$terms, ".Environment"), rlang::base_env())
   expect_identical(attr(attributes(x$model)$terms, ".Environment"), rlang::base_env())
   expect_identical(x$stanfit@.MISC, rlang::empty_env())
   expect_identical(x$stanfit@stanmodel@dso@.CXXDSOMISC, rlang::empty_env())
+  x <- axe_fitted(stanreg_fit)
   expect_equal(x$fitted.values, numeric(0))
+  x <- butcher(stanreg_fit)
   expect_equal(class(x)[1], "butchered_stanreg")
   expected_output <- predict(stanreg_fit$fit)
   expect_equal(predict(x), expected_output)

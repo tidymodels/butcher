06eedce1ab729074c4c98298bd366595cb8e0525
jyuu <jyu21@ncsu.edu>
2019-07-17 05:44:52 -0400

    Cleaned up keras documentation

--- tests/testthat/test-keras.R
+++ tests/testthat/test-keras.R
@@ -2,38 +2,6 @@ context("keras")
 
 source("utils.R")
 
-# Remove saved keras test object and test on the fly instead
-
-# test_that("keras + butcher_example() works", {
-#   example_files <- butcher_example()
-#   expect_true("binaryclass_keras.rda" %in% example_files)
-#   expect_true(file.exists(butcher_example("binaryclass_keras.rda")))
-# })
-#
-# test_that("keras class + predict() works", {
-#   skip_on_cran()
-#   skip_if_not_installed("keras")
-#   skip_if_no_keras()
-#   # Load fitted model
-#   suppressMessages(suppressWarnings(library(keras)))
-#   load(butcher_example("binaryclass_keras.rda"))
-#   suppressWarnings(keras_fit <- keras::unserialize_model(temp_object))
-#   # Load testing data
-#   load(butcher_example("testing_binaryclass_keras.rda"))
-#   predictors <- testing_data$x_test
-#   response <- testing_data$y_test
-#   # Butcher
-#   x <- butcher(keras_fit)
-#   skip_if_tensorflow_implementation()
-#   suppressWarnings(results <- keras_fit %>%
-#                      evaluate(predictors, response, verbose = 0))
-#   expect_equal(results$loss, 0.315841164593697)
-#   expect_equal(results$acc, 0.876800000667572)
-#   suppressWarnings(expected_output <- keras_fit %>%
-#                      predict(predictors[1:5, ]))
-#   expect_equal(x %>% predict(predictors[1:5, ]), expected_output)
-# })
-
 test_that("keras + predict() works", {
   skip_on_cran()
   skip_if_not_installed("keras")

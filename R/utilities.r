969f8828722ca10e4028dfb7d8aadfd5adeedff9
jyuu <jyu21@ncsu.edu>
2019-07-18 11:11:11 -0400

    New check existence and then replace function

--- R/utilities.r
+++ R/utilities.r
@@ -1,4 +1,18 @@
 
+# check existence
+exchange <- function(x, component, replacement, addition = NULL, old) {
+  out <- purrr::pluck(x, component, .default = NA)
+  if (!rlang::is_na(out)[1]) {
+    x[[component]] <- replacement
+    if (!is.null(addition) & !missing(old)) {
+      if (!is.null(old[[component]][[addition]])) {
+        x[[component]][[addition]] <- old[[component]][[addition]]
+      }
+    }
+  }
+  x
+}
+
 # butcher attributes helper
 add_butcher_disabled <- function(x, disabled = NULL) {
   current <- attr(x, "butcher_disabled")
@@ -19,9 +33,11 @@ add_butcher_class <- function(x) {
 
 # butcher attributes wrapper
 add_butcher_attributes <- function(x, old, disabled = NULL, add_class = TRUE, verbose = TRUE) {
-  x <- add_butcher_disabled(x, disabled)
-  if (add_class) {
-    x <- add_butcher_class(x)
+  if (!identical(x, old)) {
+    x <- add_butcher_disabled(x, disabled)
+    if (add_class) {
+      x <- add_butcher_class(x)
+    }
   }
   if (verbose & !missing(old)) {
     assess_object(old, x)

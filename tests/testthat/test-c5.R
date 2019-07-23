ac0a0b4d8c97aa8deedd96f7eb7e5eb0a61dd391
jyuu <jyu21@ncsu.edu>
2019-07-23 07:35:27 -0400

    Cleaned up c5 tests

--- tests/testthat/test-c5.R
+++ tests/testthat/test-c5.R
@@ -1,15 +1,11 @@
 context("c5")
 
-test_that("c5 + butcher_example() works", {
-  example_files <- butcher_example()
-  expect_true("c5.rda" %in% example_files)
-  expect_true(file.exists(butcher_example("c5.rda")))
-})
-
 test_that("c5 + predict() works", {
   skip_on_cran()
   skip_if_not_installed("C50")
-  load(butcher_example("c5.rda"))
+  library(C50)
+  data(churn)
+  c5_fit <- C5.0(x = churnTrain[, -20], y = churnTrain$churn)
   x <- axe_call(c5_fit)
   expect_equal(x$call, rlang::expr(dummy_call()))
   x <- axe_ctrl(c5_fit)
@@ -17,23 +13,8 @@ test_that("c5 + predict() works", {
   x <- axe_fitted(c5_fit)
   expect_equal(x$output, character(0))
   x <- butcher(c5_fit)
-  new_data <- data.frame(Age = c(42, 71), Number = c(7, 3), Start = c(6, 5))
-  expect_equal(predict(x, new_data),
-               structure(1:2, .Label = c("absent", "present"), class = "factor"))
-  expected_output <- predict(c5_fit$fit, new_data, type = "prob")
-  expect_equal(predict(x, new_data, type = "prob"), expected_output)
-})
-
-test_that("c5 + predict() works", {
-  skip_on_cran()
-  skip_if_not_installed("C50")
-  load(butcher_example("c5_boost.rda"))
-  x <- butcher(boost_fit)
-  expect_equal(x$call, rlang::expr(dummy_call()))
-  expect_equal(x$control, list(NULL))
-  new_data <- iris[1:2, 1:4]
-  expect_equal(predict(x, new_data),
-               structure(c(1L, 1L), .Label = c("setosa", "versicolor", "virginica"), class = "factor"))
-  expected_output <- predict(boost_fit$fit, new_data, type = "prob")
-  expect_equal(predict(x, new_data, type = "prob"), expected_output)
+  expect_equal(predict(x, churnTest),
+               predict(c5_fit, churnTest))
+  expect_equal(predict(x, churnTest, type = "prob"),
+               predict(c5_fit, churnTest, type = "prob"))
 })

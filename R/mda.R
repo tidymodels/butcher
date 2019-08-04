c1ba46924874bb7ca1b4d4d5f6650cccded3170e
jyuu <jyu21@ncsu.edu>
2019-08-04 13:39:08 -0400

    New axe methods and tests for mda objects

---
+++ R/mda.R
@@ -0,0 +1,63 @@
+#' Axing a mda.
+#'
+#' This is where all the mda specific documentation lies.
+#'
+#' @param x Model object.
+#' @param verbose Print information each time an axe method is executed
+#'  that notes how much memory is released and what functions are
+#'  disabled. Default is \code{FALSE}.
+#' @param ... Any additional arguments related to axing.
+#'
+#' @return Axed model object.
+#' @examples
+#' ##
+#' ## Insert examples to create and axe model object here...
+#' ##
+#' @name axe-mda
+NULL
+
+#' Remove the call.
+#'
+#' @rdname axe-mda
+#' @export
+axe_call.mda <- function(x, verbose = FALSE, ...) {
+  old <- x
+  x <- exchange(x, "call", call("dummy_call"))
+
+  add_butcher_attributes(
+    x,
+    old,
+    disabled = c("print()", "summary()", "update()"),
+    verbose = verbose
+  )
+}
+
+#' Remove environments.
+#'
+#' @rdname axe-mda
+#' @export
+axe_env.mda <- function(x, verbose = FALSE, ...) {
+  old <- x
+  x$terms <- axe_env(x$terms, ...)
+
+  add_butcher_attributes(
+    x,
+    old,
+    verbose = verbose
+  )
+}
+
+#' Remove fitted values.
+#'
+#' @rdname axe-mda
+#' @export
+axe_fitted.mda <- function(x, verbose = FALSE, ...) {
+  old <- x
+  x$fit <- exchange(x$fit, "fitted.values", matrix(NA))
+
+  add_butcher_attributes(
+    x,
+    old,
+    verbose = verbose
+  )
+}
---
+++ tests/testthat/test-mda.R
@@ -0,0 +1,82 @@
+context("mda")
+
+test_that("mda + predict() works", {
+  skip_on_cran()
+  skip_if_not_installed("mda")
+  library(mda)
+  fit <- mda(Species ~ ., data = iris)
+  x <- axe_call(fit)
+  expect_equal(x$call, rlang::expr(dummy_call()))
+  expect_error(update(x, subclasses = 4))
+  expect_equal(attr(x, "butcher_disabled"),
+               c("print()", "summary()", "update()"))
+  x <- axe_data(fit)
+  expect_identical(x, fit)
+  x <- axe_env(fit)
+  expect_identical(attr(x$terms, ".Environment"), rlang::base_env())
+  x <- axe_fitted(fit)
+  expect_equal(x$fit$fitted.values, matrix(NA))
+  x <- butcher(fit)
+  expect_equal(predict(x, iris[1:3, ]),
+               predict(fit, iris[1:3, ]))
+})
+
+test_that("mda + custom parsnip model + predict() works", {
+  skip_on_cran()
+  skip_if_not_installed("mda")
+  skip_if_not_installed("parsnip")
+  library(mda)
+  library(parsnip)
+  # Create a custom parsnip model using mda engine
+  set_new_model("mixture_da")
+  set_model_mode(model = "mixture_da", mode = "classification")
+  set_model_engine(
+    "mixture_da",
+    mode = "classification",
+    eng = "mda"
+  )
+  set_model_arg(
+    model = "mixture_da",
+    eng = "mda",
+    parsnip = "sub_classes",
+    original = "subclasses",
+    func = list(pkg = "foo", fun = "bar"),
+    has_submodel = FALSE
+  )
+  mixture_da <- function(mode = "classification",  sub_classes = NULL) {
+    if (mode  != "classification") {
+      stop("`mode` should be 'classification'", call. = FALSE)
+    }
+    args <- list(sub_classes = rlang::enquo(sub_classes))
+    out <- list(args = args,
+                eng_args = NULL,
+                mode = mode,
+                method = NULL,
+                engine = NULL)
+    class(out) <- make_classes("mixture_da")
+    out
+  }
+  set_fit(
+    model = "mixture_da",
+    eng = "mda",
+    mode = "classification",
+    value = list(
+      interface = "formula",
+      protect = c("formula", "data"),
+      func = c(pkg = "mda", fun = "mda"),
+      defaults = list()
+    )
+  )
+  mda_fit <- mixture_da(sub_classes = 2) %>%
+    set_engine("mda") %>%
+    fit(Species ~ ., data = iris)
+  x <- axe_call(mda_fit)
+  expect_equal(x$call, rlang::expr(dummy_call()))
+  expect_error(update(x, subclasses = 4))
+  expect_equal(attr(x, "butcher_disabled"),
+               c("print()", "summary()", "update()"))
+  x <- axe_env(mda_fit)
+  expect_identical(attr(x$terms, ".Environment"), rlang::base_env())
+  x <- axe_fitted(mda_fit)
+  expect_equal(x$fit$fitted.values, matrix(NA))
+})

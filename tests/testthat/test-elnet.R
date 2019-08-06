b3e0ad6ab22dce445ffcfd735a4ab1f30e8656cd
jyuu <jyu21@ncsu.edu>
2019-08-06 08:20:53 -0400

    Removed large elnet rda, and updated respective unit test

--- tests/testthat/test-elnet.R
+++ tests/testthat/test-elnet.R
@@ -1,21 +1,17 @@
 context("elnet")
 
-test_that("elnet + butcher_example() works", {
-  example_files <- butcher_example()
-  expect_true("elnet.rda" %in% example_files)
-  expect_true(file.exists(butcher_example("elnet.rda")))
-})
-
 test_that("elnet + predict() works", {
   skip_on_cran()
   skip_if_not_installed("glmnet")
-  load(butcher_example("elnet.rda"))
+  library(glmnet)
+  elnet_fit <- glmnet(x = as.matrix(mtcars[, c(1, 5, 6)]),
+                      y = mtcars$hp,
+                      alpha = 0)
   x <- butcher(elnet_fit)
-  new_data <- as.matrix(mtcars[1:3, 2:11])
-  expect_equal(predict(x, new_data)[1], 22.5177880899825)
+  new_data <- as.matrix(mtcars[1:3, c(1, 5, 6)])
+  expect_equal(predict(x, new_data),
+               predict(elnet_fit, new_data))
   x <- axe_call(elnet_fit)
-  expect_equal(x$call, rlang::expr(dummy_call()))
-  x <- axe_call(elnet_fit$fit)
   expect_equal(x$call, rlang::expr(dummy_call()))
 })
 

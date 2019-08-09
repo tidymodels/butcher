cda10ecb6a13c0c4e12e4e12346fb1c3c5795a58
jyuu <jyu21@ncsu.edu>
2019-08-08 22:26:11 -0400

    Added elnet example

--- R/elnet.R
+++ R/elnet.R
@@ -11,6 +11,7 @@
 #' # Load libraries
 #' suppressWarnings(suppressMessages(library(parsnip)))
 #' suppressWarnings(suppressMessages(library(rsample)))
+#' suppressWarnings(suppressMessages(library(glmnet)))
 #'
 #' # Load data
 #' split <- initial_split(mtcars, props = 9/10)
@@ -22,7 +23,18 @@
 #'   set_engine("glmnet") %>%
 #'   fit_xy(x = car_train[, 2:11], y = car_train[, 1, drop = FALSE])
 #'
-#' butcher(elnet_fit)
+#' out <- butcher(elnet_fit, verbose = TRUE)
+#'
+#' # Another elnet model object
+#' x <- matrix(rnorm(100*20), 100, 20)
+#' y <- rnorm(100)
+#' fit <- glmnet(x,y)
+#' out <- butcher(fit)
+#'
+#' # Prediction still works
+#' original_prediction <- predict(fit, x[c(1, 8, 11), ])
+#' butchered_prediction <- predict(out, x[c(1, 8, 11), ])
+#' identical(original_prediction, butchered_prediction)
 #'
 #' @name axe-elnet
 NULL

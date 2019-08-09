6341bd4a6c0cf3a5622650a946e59b6c6ee03000
jyuu <jyu21@ncsu.edu>
2019-08-08 22:26:00 -0400

    Added cv example

--- R/cv.R
+++ R/cv.R
@@ -10,19 +10,42 @@
 #' @examples
 #' # Load libraries
 #' library(glmnet)
-#' n <- 500;p <- 30
+#'
+#' # Example 1
+#' n <- 500
+#' p <- 30
 #' nzc <- trunc(p/10)
 #' x <- matrix(rnorm(n*p), n, p)
-#' beta3 <- matrix(rnorm(30), 10, nzc)
-#' beta3 <- rbind(beta3, matrix(0, p-10, nzc))
-#' f3 <- x %*% beta3
-#' p3 <- exp(f3)
-#' p3 <- p3/apply(p3, 1, sum)
-#' g3 <- rmult(p3)
+#' beta <- matrix(rnorm(30), 10, nzc)
+#' beta <- rbind(beta, matrix(0, p-10, nzc))
+#' f <- x %*% beta
+#' p <- exp(f)
+#' p <- p/apply(p, 1, sum)
+#' g <- rmult(p)
 #' set.seed(10101)
-#' cvfit <- cv.glmnet(x, g3, family="multinomial", keep = TRUE)
+#' cvfit <- cv.glmnet(x, g, family="multinomial", keep = TRUE)
 #'
-#' butcher(cvfit)
+#' out <- butcher(cvfit, verbose = TRUE)
+#'
+#' # Example 2
+#' n <- 1000
+#' p <- 100
+#' nzc <- trunc(p/10)
+#' x <- matrix(rnorm(n*p), n, p)
+#' beta <- rnorm(nzc)
+#' fx <- x[, seq(nzc)] %*% beta
+#' eps <- rnorm(n)*5
+#' y <- drop(fx+eps)
+#' px <- exp(fx)
+#' px <- px/(1+px)
+#' ly <- rbinom(n = length(px), prob = px, size = 1)
+#' cvfit2 <- cv.glmnet(x, ly,
+#'                     family = "binomial",
+#'                     type.measure = "auc",
+#'                     keep = TRUE)
+#'
+#' out <- butcher(cvfit2, verbose = TRUE)
+#'
 #' @name axe-cv.glmnet
 NULL
 

8258821a7cbadc0d0a1304614dfe83766efcf0e7
Adrian Serapio <adtserapio@gmail.com>
2019-12-09 13:25:19 +0800

    Removed Example Files

--- tests/testthat/test-example.R
+++
@@ -1,6 +0,0 @@
-context("example")
-
-test_that("default example is returned", {
-  expected_file <- dir(system.file("extdata", package = "butcher"))
-  expect_equal(butcher_example(NULL), expected_file)
-})

b66919cd2c9aa99b67c0c8db79d0172bc48da3e8
jyuu <jyu21@ncsu.edu>
2020-01-03 15:03:03 -0500

    Added back multnet axe methods and respective tests

---
+++ R/multnet.R
@@ -0,0 +1,43 @@
+#' Axing an multnet.
+#'
+#' multnet objects are created from carrying out multinomial regression
+#' in the \pkg{glmnet} package.
+#'
+#' @inheritParams butcher
+#'
+#' @return Axed multnet object.
+#'
+#' @examples
+#' # Load libraries
+#' suppressWarnings(suppressMessages(library(parsnip)))
+#'
+#' # Load data
+#' set.seed(1234)
+#' predictrs <- matrix(rnorm(100*20), ncol = 20)
+#' response <- as.factor(sample(1:4, 100, replace = TRUE))
+#'
+#' # Create model and fi
+#' multnet_fit <- multinom_reg() %>%
+#'   set_engine("glmnet") %>%
+#'   fit_xy(x = predictrs, y = response)
+#'
+#' out <- butcher(multnet_fit, verbose = TRUE)
+#'
+#' @name axe-multnet
+NULL
+
+#' Remove call.
+#'
+#' @rdname axe-multnet
+#' @export
+axe_call.multnet <- function(x, verbose = FALSE, ...) {
+  old <- x
+  x <- exchange(x, "call", call("dummy_call"))
+
+  add_butcher_attributes(
+    x,
+    old,
+    disabled = c("print()", "summary()"),
+    verbose = verbose
+  )
+}
---
+++ tests/testthat/test-multnet.R
@@ -0,0 +1,22 @@
+context("multnet")
+
+test_that("multnet + predict() works", {
+  skip_on_cran()
+  skip_if(do_not_run_glmnet)
+  skip_if_not_installed("glmnet")
+  library(parsnip)
+  set.seed(1234)
+  predictrs <- matrix(rnorm(100*20), ncol = 20)
+  response <- as.factor(sample(1:4, 100, replace = TRUE))
+  fit <- multinom_reg() %>%
+    set_engine("glmnet") %>%
+    fit_xy(x = predictrs, y = response)
+  x <- axe_call(fit)
+  expect_equal(x$call, rlang::expr(dummy_call()))
+  x <- butcher(fit)
+  expect_equal(
+    predict(fit, new_data = predictrs[1:3, ], penalty = 1),
+    structure(
+      list(.pred_class = structure(c(3L, 3L, 3L), .Label = c("1", "2", "3", "4"), class = "factor")), row.names = c(NA, -3L), class = c("tbl_df", "tbl", "data.frame"))
+  )
+})

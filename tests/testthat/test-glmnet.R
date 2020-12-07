6b2ac4b9781ced1e10405c1e4a2d6f2a5a9bf677
DavisVaughan <davis@rstudio.com>
2020-12-07 17:52:15 -0500

    Fix typo in glmnet test

--- tests/testthat/test-glmnet.R
+++ tests/testthat/test-glmnet.R
@@ -8,6 +8,6 @@ test_that("glmnet + predict() works", {
   model <- logistic_reg(penalty = 10, mixture = 0.1) %>%
     set_engine("glmnet") %>%
     fit(as.factor(vs) ~ ., data = mtcars)
-  axed <- axe_call(fit)
+  axed <- axe_call(model)
   expect_equal(axed$call, rlang::expr(dummy_call()))
 })

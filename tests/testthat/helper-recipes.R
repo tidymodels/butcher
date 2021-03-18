edf4cfedeeddb37fcc9d38934745a1beecee41c8
DavisVaughan <davis@rstudio.com>
2021-03-18 13:53:01 -0400

    Add helpers for skipping on different recipes versions

---
+++ tests/testthat/helper-recipes.R
@@ -0,0 +1,13 @@
+skip_if_recipes_pre_0.1.16 <- function() {
+  skip_if(
+    condition = packageVersion("recipes") < "0.1.15.9000",
+    message = "Skipping on old recipes (pre 0.1.16)"
+  )
+}
+
+skip_if_recipes_post_0.1.16 <- function() {
+  skip_if(
+    condition = packageVersion("recipes") >= "0.1.15.9000",
+    message = "Skipping on new recipes where no longer applicable (post 0.1.16)"
+  )
+}

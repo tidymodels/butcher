2f4a077465a61a1022ddbb5e921fe8d11efad3f6
Julia Silge <julia.silge@gmail.com>
2022-11-28 11:42:24 -0700

    Use smaller data in example, add class, exchange to empty-er data structures

--- R/kproto.R
+++ R/kproto.R
@@ -4,76 +4,49 @@
 #'
 #' @return Axed kproto object.
 #'
-#' @examples
+#' @examplesIf rlang::is_installed("clustMixType")
 #' library(clustMixType)
 #'
-#' ## BEGIN: Generate data
-#' # toy data set example from clustMixType::kproto
-#' n   <- 100
-#' prb <- 0.9
-#' muk <- 1.5
-#' clusid <- rep(1:4, each = n)
+#' kproto_fit <- kproto(
+#'   ToothGrowth,
+#'   k = 2,
+#'   lambda = lambdaest(ToothGrowth),
+#'   verbose = FALSE
+#' )
 #'
-#' x1 <- sample(c("A","B"), 2*n, replace = TRUE, prob = c(prb, 1-prb))
-#' x1 <- c(x1, sample(c("A","B"), 2*n, replace = TRUE, prob = c(1-prb, prb)))
-#' x1 <- as.factor(x1)
-#'
-#' x2 <- sample(c("A","B"), 2*n, replace = TRUE, prob = c(prb, 1-prb))
-#' x2 <- c(x2, sample(c("A","B"), 2*n, replace = TRUE, prob = c(1-prb, prb)))
-#' x2 <- as.factor(x2)
-#'
-#' x3 <- c(rnorm(n, mean = -muk), rnorm(n, mean = muk), rnorm(n, mean = -muk), rnorm(n, mean = muk))
-#' x4 <- c(rnorm(n, mean = -muk), rnorm(n, mean = muk), rnorm(n, mean = -muk), rnorm(n, mean = muk))
-#'
-#' x <- data.frame(x1,x2,x3,x4)
-#' ## END: Generate data
-#'
-#' kproto_model <- clustMixType::kproto(x = x,
-#'                                      k=2,
-#'                                      iter.max = 1000,
-#'                                      nstart = 10,
-#'                                      lambda = clustMixType::lambdaest(x = x,
-#'                                                                       num.method = 1,
-#'                                                                       fac.method = 1),
-#'                                      verbose = FALSE)
-#'
-#' streamlined_kproto <- butcher(kproto_model, verbose = TRUE)
+#' out <- butcher(kproto_fit, verbose = TRUE)
 #'
 #' @name axe-kproto
 NULL
 
-#' Remove the training data.
-#'
 #' @rdname axe-kproto
 #' @export
 axe_data.kproto <- function(x, verbose = FALSE, ...) {
   old <- x
-  x <- exchange(x, "data", old$data[FALSE,])
+  x <- exchange(x, "data", data.frame(NA))
   # none of the validation methods in validation_kproto() work if the data is
   # not stored within the model object
   add_butcher_attributes(
     x,
     old,
     disabled = c("validation_kproto()", "summary()"),
-    add_class = FALSE,
+    add_class = TRUE,
     verbose = verbose
   )
 }
 
-#' Remove fitted values.
-#'
 #' @rdname axe-kproto
 #' @export
 axe_fitted.kproto <- function(x, verbose = FALSE, ...) {
   old <- x
   x <- exchange(x, "cluster", integer(0))
-  x <- exchange(x, "dists", old$dists[FALSE,,drop=FALSE])
+  x <- exchange(x, "dists", matrix(NA_real_))
 
   add_butcher_attributes(
     x,
     old,
     disabled = c("clprofiles()", "validation_kproto()"),
-    add_class = FALSE,
+    add_class = TRUE,
     verbose = verbose
   )
 }

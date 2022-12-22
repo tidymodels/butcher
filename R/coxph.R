b256e98aca4a59432a8671efd4f5949598a616fa
simonpcouch <simonpatrickcouch@gmail.com>
2022-12-22 10:53:45 -0500

    add butcher methods for `survival::coxph()`
    There's an environment on the terms object that is quite "heavy," though coxph errors out if it's reset with `base_env()`.

---
+++ R/coxph.R
@@ -0,0 +1,62 @@
+#' Axing a coxph.
+#'
+#' @inheritParams butcher
+#'
+#' @return Axed coxph object.
+#'
+#' @examplesIf rlang::is_installed("survival")
+#' library(survival)
+#'
+#' example_data <-
+#'   tibble::tibble(
+#'     time = rpois(1000, 2) + 1,
+#'     status = rbinom(1000, 1, .5),
+#'     x = rpois(1000, .5),
+#'     covar = rbinom(1000, 1, .5)
+#'   )
+#'
+#' example_data
+#'
+#' make_big_model <- function() {
+#'   boop <- runif(1e6)
+#'   coxph(Surv(time, status) ~ x + strata(covar), example_data)
+#' }
+#'
+#' res <- make_big_model()
+#'
+#' weigh(res)
+#' weigh(butcher(res))
+#'
+#' @name axe-coxph
+NULL
+
+#' @rdname axe-coxph
+#' @export
+axe_env.coxph <- function(x, verbose = FALSE, ...) {
+  res <- x
+  #res$terms <- axe_env(res$terms, verbose = verbose, ...)
+  #attr(res$terms, ".Environment") <- rlang::base_env()
+  #attributes(res$model)$terms <- axe_env(attributes(res$model)$terms, ...)
+  res$formula <- axe_env(res$formula, verbose = verbose, ...)
+
+  add_butcher_attributes(
+    res,
+    x,
+    add_class = TRUE,
+    verbose = verbose
+  )
+}
+
+#' @rdname axe-coxph
+#' @export
+axe_data.coxph <- function(x, verbose = FALSE, ...) {
+  res <- x
+  res <- exchange(res, "y", numeric(0))
+
+  add_butcher_attributes(
+    res,
+    x,
+    add_class = TRUE,
+    verbose = verbose
+  )
+}

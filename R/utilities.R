5065803dac1807c692b79c79c4797f85f3509c58
Julia Silge <julia.silge@gmail.com>
2023-08-22 15:54:37 -0600

    Don't run mixOmics example on CRAN

--- R/utilities.R
+++ R/utilities.R
@@ -44,3 +44,16 @@ add_butcher_attributes <- function(x, old, disabled = NULL, add_class = TRUE, ve
   }
   x
 }
+
+# adapted from ps:::is_cran_check()
+# nocov start
+
+is_cran_check <- function () {
+  if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+    FALSE
+  } else {
+    Sys.getenv("_R_CHECK_PACKAGE_NAME_", "") != ""
+  }
+}
+
+#nocov end

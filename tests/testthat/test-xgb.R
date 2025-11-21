8b1b4681e4b1338c6b06dfda743084a2fc3674bd
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-11-20 18:23:51 -0800

    update xgb.Booster methods to work with newer versions of butcher

--- tests/testthat/test-xgb.R
+++ tests/testthat/test-xgb.R
@@ -9,18 +9,34 @@ test_that("xgb.Booster + linear solver + predict() works", {
   # Load data
   data(agaricus.train)
   data(agaricus.test)
-  bst <- xgboost(data = agaricus.train$data,
-                 label = agaricus.train$label,
-                 eta = 1,
-                 nthread = 2,
-                 nrounds = 2,
-                 eval_metric = "logloss",
-                 objective = "binary:logistic",
-                 verbose = 0)
+  if (utils::packageVersion("xgboost") > "2.0.0.0") {
+    bst <- xgboost(x = agaricus.train$data,
+                   y = agaricus.train$label,
+                   learning_rate = 1,
+                   nthread = 2,
+                   nrounds = 2,
+                   eval_metric = "logloss",
+                   objective = "reg:squarederror")
+  } else {
+    bst <- xgboost(data = agaricus.train$data,
+                   label = agaricus.train$label,
+                   eta = 1,
+                   nthread = 2,
+                   nrounds = 2,
+                   eval_metric = "logloss",
+                   objective = "binary:logistic",
+                   verbose = 0)
+  }
   x <- axe_call(bst)
-  expect_equal(x$call, rlang::expr(dummy_call()))
+  if (utils::packageVersion("xgboost") > "2.0.0.0") {
+    extracted_call <- attr(x, "call")  
+  } else {
+    extracted_call <- x$call
+  }
+  expect_equal(extracted_call, rlang::expr(dummy_call()))
   x <- axe_env(bst)
-  expect_lt(lobstr::obj_size(x), lobstr::obj_size(bst))
+  expect_lte(lobstr::obj_size(x), lobstr::obj_size(bst))
+  expect_lte(lobstr::obj_size(attributes(x)), lobstr::obj_size(attributes(bst)))
   x <- butcher(bst)
   expect_equal(xgb.importance(model = x),
                xgb.importance(model = bst))
@@ -28,7 +44,7 @@ test_that("xgb.Booster + linear solver + predict() works", {
                predict(bst, agaricus.test$data))
   expect_equal(xgb.dump(x, with_stats = TRUE),
                xgb.dump(bst, with_stats = TRUE))
-})
+    })
 
 test_that("xgb.Booster + tree-learning algo + predict() works", {
   skip_on_cran()
@@ -39,17 +55,37 @@ test_that("xgb.Booster + tree-learning algo + predict() works", {
   data(agaricus.test)
   dtrain <- xgb.DMatrix(data = agaricus.train$data,
                         label = agaricus.train$label)
-  bst <- xgb.train(data = dtrain,
-                   booster = "gblinear",
-                   nthread = 2,
-                   nrounds = 2,
-                   eval_metric = "logloss",
-                   objective = "binary:logistic",
-                   print_every_n = 10000L)
+  if (utils::packageVersion("xgboost") > "2.0.0.0") {
+    bst <- xgb.train(
+      params = list(
+        booster = "gblinear",
+        nthread = 2,
+        eval_metric = "logloss",
+        objective = "binary:logistic",
+        print_every_n = 10000L
+      ),
+      nrounds = 2,
+      data = dtrain
+    )
+  } else {
+    bst <- xgb.train(data = dtrain,
+                    booster = "gblinear",
+                    nthread = 2,
+                    nrounds = 2,
+                    eval_metric = "logloss",
+                    objective = "binary:logistic",
+                    print_every_n = 10000L)   
+  }
   x <- axe_call(bst)
-  expect_equal(x$call, rlang::expr(dummy_call()))
+  if (utils::packageVersion("xgboost") > "2.0.0.0") {
+    extracted_call <- attr(x, "call")  
+  } else {
+    extracted_call <- x$call
+  }
+  expect_equal(extracted_call, rlang::expr(dummy_call()))
   x <- axe_env(bst)
-  expect_lt(lobstr::obj_size(x), lobstr::obj_size(bst))
+  expect_lte(lobstr::obj_size(x), lobstr::obj_size(bst))
+  expect_lte(lobstr::obj_size(attributes(x)), lobstr::obj_size(attributes(bst)))
   x <- butcher(bst)
   expect_equal(xgb.importance(model = x),
                xgb.importance(model = bst))
@@ -57,4 +93,4 @@ test_that("xgb.Booster + tree-learning algo + predict() works", {
                predict(bst, agaricus.test$data))
   expect_equal(xgb.dump(x, with_stats = TRUE),
                xgb.dump(bst, with_stats = TRUE))
-})
+    })
